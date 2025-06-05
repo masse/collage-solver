a99026b5d48b2d07bcc8dab2b74cb99443fa3f0b
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2025-06-05 13:30:25 +0200

    Add more tests

--- src/test/kotlin/se/kodverket/collage/layoutsolver/LayoutSolutionTest.kt
+++ src/test/kotlin/se/kodverket/collage/layoutsolver/LayoutSolutionTest.kt
@@ -65,6 +65,147 @@ class LayoutSolutionTest {
         )
     }
 
+    @Test
+    fun `it should penalize undersized feature images more than oversized ones`() {
+        // Create a base configuration
+        val baseConfig =
+            CollageConfig(
+                targetWidth = 800,
+                targetHeight = 600,
+                scoringFactors = ScoringFactors(0.0, 1.0, 0.0) // Only consider relative image size
+            )
+
+        // Create images with different sizes
+        val smallImage = SourceImage("small.png", Dimension(100.0, 100.0), 2) // Feature image (weight > 1)
+        val largeImage = SourceImage("large.png", Dimension(400.0, 400.0), 2) // Feature image (weight > 1)
+
+        // Create layouts with undersized and oversized feature images
+        val undersizedLayout =
+            createCustomLayout(
+                smallImage,
+                SourceImage("filler.png", Dimension(300.0, 300.0), 1),
+                baseConfig
+            )
+
+        val oversizedLayout =
+            createCustomLayout(
+                largeImage,
+                SourceImage("filler.png", Dimension(100.0, 100.0), 1),
+                baseConfig
+            )
+
+        // Score both layouts
+        val undersizedScore = undersizedLayout.score().score
+        val oversizedScore = oversizedLayout.score().score
+
+        // Undersized feature images should be penalized more (higher score is worse)
+        (undersizedScore > oversizedScore) shouldBe true
+    }
+
+    @Test
+    fun `it should consider off-center distance for feature images`() {
+        // Create configurations with different scoring factors
+        val withCenteringConfig =
+            CollageConfig(
+                targetWidth = 800,
+                targetHeight = 600,
+                scoringFactors = ScoringFactors(0.0, 0.0, 1.0) // Only consider centering
+            )
+
+        val withoutCenteringConfig =
+            CollageConfig(
+                targetWidth = 800,
+                targetHeight = 600,
+                scoringFactors = ScoringFactors(0.0, 0.0, 0.0) // Ignore centering
+            )
+
+        // Create a feature image
+        val featureImage = SourceImage("feature.png", Dimension(200.0, 200.0), 2)
+        val fillerImage = SourceImage("filler.png", Dimension(200.0, 200.0), 1)
+
+        // Create layouts with the same images but different configs
+        val layoutWithCentering = createCustomLayout(featureImage, fillerImage, withCenteringConfig)
+        val layoutWithoutCentering = createCustomLayout(featureImage, fillerImage, withoutCenteringConfig)
+
+        // Score both layouts
+        val scoreWithCentering = layoutWithCentering.score().score
+        val scoreWithoutCentering = layoutWithoutCentering.score().score
+
+        // Score with centering should be higher (worse) than without
+        (scoreWithCentering > scoreWithoutCentering) shouldBe true
+    }
+
+    @Test
+    fun `it should consider canvas coverage`() {
+        // Create configurations with different scoring factors
+        val withCoverageConfig =
+            CollageConfig(
+                targetWidth = 800,
+                targetHeight = 600,
+                scoringFactors = ScoringFactors(1.0, 0.0, 0.0) // Only consider coverage
+            )
+
+        val withoutCoverageConfig =
+            CollageConfig(
+                targetWidth = 800,
+                targetHeight = 600,
+                scoringFactors = ScoringFactors(0.0, 0.0, 0.0) // Ignore coverage
+            )
+
+        // Create small images that won't cover the canvas well
+        val image1 = SourceImage("small1.png", Dimension(100.0, 100.0), 1)
+        val image2 = SourceImage("small2.png", Dimension(100.0, 100.0), 1)
+
+        // Create layouts with the same images but different configs
+        val layoutWithCoverage = createCustomLayout(image1, image2, withCoverageConfig)
+        val layoutWithoutCoverage = createCustomLayout(image1, image2, withoutCoverageConfig)
+
+        // Score both layouts
+        val scoreWithCoverage = layoutWithCoverage.score().score
+        val scoreWithoutCoverage = layoutWithoutCoverage.score().score
+
+        // Score with coverage should be higher (worse) than without
+        (scoreWithCoverage > scoreWithoutCoverage) shouldBe true
+    }
+
+    @Test
+    fun `it should handle extreme image size differences`() {
+        // Create a base configuration
+        val config =
+            CollageConfig(
+                targetWidth = 800,
+                targetHeight = 600,
+                scoringFactors = ScoringFactors(0.0, 1.0, 0.0) // Only consider relative image size
+            )
+
+        // Create images with extreme size differences
+        val tinyImage = SourceImage("tiny.png", Dimension(10.0, 10.0), 1)
+        val hugeImage = SourceImage("huge.png", Dimension(1000.0, 1000.0), 1)
+
+        // Create a layout with these extreme images
+        val layout = createCustomLayout(tinyImage, hugeImage, config)
+
+        // Score the layout
+        val score = layout.score().score
+
+        // Score should be positive (there should be some penalty)
+        (score > 0.0) shouldBe true
+    }
+
+    private fun createCustomLayout(
+        image1: SourceImage,
+        image2: SourceImage,
+        config: CollageConfig,
+    ): LayoutSolution {
+        val sourceImages = listOf(image1, image2)
+        val imageNode1 = ImageNode(image1)
+        val imageNode2 = ImageNode(image2)
+
+        val conf = config.copy(desiredRelativeWeightSum = sourceImages.sumOf { it.desiredRelativeWeight })
+        val rootNode = LayoutNode(slicingDirection = SlicingDirection.H, left = imageNode1, right = imageNode2)
+        return LayoutSolution(rootNode, conf)
+    }
+
     private fun createScoredIndividual(config: CollageConfig): ScoredIndividual<LayoutSolution> {
         val image1 = SourceImage("image1.png", Dimension(500.0, 600.0), 1)
         val image2 = SourceImage("image2.png", Dimension(200.0, 300.0), 1)
